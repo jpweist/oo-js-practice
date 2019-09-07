ea29b6d9df9ceac583d4ccfca97f771386ffb2bc
Jim Weist <jp_weist@yahoo.com>
2019-09-07 11:25:35 -0600

    I got stuck on the js line 11

--- objects-and-functions/lib/gradebook.js
+++ objects-and-functions/lib/gradebook.js
@@ -1,1 +1,18 @@
+class Course {
+  constructor(className, level) {
+    this.className = className;
+    this.level = level;
+  }
+}
 
+
+class GradeBook {
+  constructor(course, instructor) {
+    this.course = course;
+    this.instructor = 'Ms. Frizzle';
+  }
+
+}
+
+
+module.exports = GradeBook;
--- objects-and-functions/lib/student.js
+++ objects-and-functions/lib/student.js
@@ -1,1 +1,15 @@
+class Student {
+  constructor(object) {
+    this.name = object.name;
+    this.age = object.age;
+    this.scores = [];
+    this.averageGrade = 0;
+  }
+  logScore(score) {
+    this.scores.push(score);
+    this.averageGrade = (this.scores[0] + this.scores[1]) / 2;
 
+  }
+
+}
+module.exports = Student;
